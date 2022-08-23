c01ab142b45c1a2aa794d3a222465e2c1fee1345
Stefano Morgia <ste.morgia@gmail.com>
2022-08-23 17:37:08 +0200

    esercizio completato

---
+++ src/components/MyTitle.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template> 
+
+<script>
+
+export default {
+    name: 'MyTitle',
+    
+}
+</script>
+
+<style lang="scss">
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    }
+</style>
